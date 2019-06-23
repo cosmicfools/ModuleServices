d44f1dfa79c08aa8b02740e28f0c2192b94237c8
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2019-06-23 16:22:26 +0200

    Use combination-generator for cover some scenarios in the configurableCell

--- FTMTableSectionModules/Classes/Test/SnapshotObject.swift
+++ FTMTableSectionModules/Classes/Test/SnapshotObject.swift
@@ -9,4 +9,15 @@ import UIKit
 
 open class SnapshotObject: NSObject {
     open var orientation: UIInterfaceOrientation?
+    
+    open func snapshotIdentifier() -> String {
+        var identififer: String = String(describing: self)
+        let mirror = Mirror(reflecting: self)
+        
+        for child in mirror.children {
+            identififer += (child.label! + (child.value as! String))
+        }
+        
+        return identififer
+    }
 }
